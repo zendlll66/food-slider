b8444ef08b8cf59924af1e77b3ba0b8d3915ea75
ZEnd <om99305@gmail.com>
2025-04-28 20:29:27 +0700

    fix responsive

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -30,7 +30,20 @@ const Homepage = () => {
     const [currentIndex, setCurrentIndex] = useState(0); // state สำหรับ index ของภาพที่หมุนอยู่
     const totalImages = images.length;
     const angleStep = 360 / totalImages; // ขนาดมุมแต่ละภาพ
+    const getRadius = () => {
+        const width = window.innerWidth;
+        if (width >= 1024) {
+            return 400;
+        } else if (width >= 768) {
+            return 500;
+        } else {
+            return 200;
+        }
+    };
 
+    const [radius, setRadius] = useState(getRadius());
+
+    
     const rotateLeft = () => {
         setRotation((prev) => {
             const newRotation = (prev - angleStep + 360) % 360;
@@ -49,54 +62,71 @@ const Homepage = () => {
         });
     };
 
+
+
+    // ฟังก์ชันสำหรับการอัปเดต radius เมื่อขนาดหน้าจอเปลี่ยน
+    const updateRadius = () => {
+        setRadius(window.innerWidth >= 768 ? 500 : 200);
+        setRadius(window.innerWidth >= 1024 ? 500 : 200);
+    };
+
+
     // เรียกใช้ AOS.init() เพื่อเริ่มใช้งาน AOS
     useEffect(() => {
         AOS.init({
-            duration: 1000, // ความเร็วในการแอนิเมชัน
-            easing: 'ease-in-out', // รูปแบบการเคลื่อนไหว
+            duration: 1000,
+            easing: 'ease-in-out',
         });
+
+        // ฟังก์ชัน update ขนาดหน้าจอ
+        window.addEventListener('resize', updateRadius);
+
+        return () => {
+            window.removeEventListener('resize', updateRadius);
+        };
     }, []);
 
     return (
         <div className="w-full h-screen overflow-hidden relative">
             {/* bg section */}
-            <div className="absolute z-[-10] flex flex-row w-full h-screen">
-                <div className="bg-[#F28A38] w-1/2 h-screen"></div>
-                <div className="bg-gradient-to-r from-[#24252b] to-[#10121a] w-1/2 h-screen"></div>
+            <div className="absolute z-[-10] flex  flex-col md:flex-row w-full h-screen">
+                <div className="bg-[#F28A38] md:w-1/2 md:h-screen w-full h-[50vh]"></div>
+                <div className="bg-gradient-to-r from-[#24252b] to-[#10121a] md:w-1/2 md:h-screen  w-full h-[50vh]"></div>
+
             </div>
 
             {/* Heading title and description */}
-            <div className="flex flex-col">
-                <div className="flex flex-row">
-                    <h1 className="font-gochi text-9xl w-1/2 p-20 text-end" data-aos="fade-right">
+            <div className="flex flex-col ">
+                <div className="flex md:flex-row flex-col justify-center items-center md:items-start text-center h-[50vh] md:h-[50vh]">
+                    <h1 className="text-center font-gochi lg:text-9xl md:text-7xl  text-white md:items-start  text-4xl w-1/2 md:p-20 p-10 md:text-end flex-grow" data-aos="fade-right">
                         Food <br /> Slider !
                     </h1>
 
-                    <div className="w-1/2 text-white p-20 space-y-10 " data-aos="fade-left">
-                        <h1 className="text-5xl transition-all duration-500">{titles[currentIndex]}</h1>
-                        <p className='transition-all duration-500'>{descriptions[currentIndex]}</p>
-                        <button className="bg-[#F28A38] border-[1px] border-white border-b-4 shadow-lg/30 hover:scale-125 transition-all duration-400 shadow-[#F28A38] p-2 rounded-2xl text-[#10121a]">
+                    <div className="w-1/2 text-white md:text-start md:p-20 md:space-y-10 space-y-5 flex-grow" data-aos="fade-left">
+                        <h1 className="md:text-5xl transition-all duration-500 md:text-left">{titles[currentIndex]}</h1>
+                        <p className='transition-all  md:text-left duration-500 text-[12px] md:text-md'>{descriptions[currentIndex]}</p>
+                        <button className="bg-[#F28A38]   border-[1px] border-white border-b-4 shadow-lg/30 hover:scale-125 transition-all duration-400 shadow-[#F28A38] p-2 rounded-2xl text-[#10121a]">
                             check it!!
                         </button>
                     </div>
                 </div>
 
+
+
                 {/* ปุ่มหมุน */}
                 <div className='absolute flex z-30 w-full top-1/2' data-aos="fade-up">
-                    <div className='flex flex-row items-center w-full justify-center space-x-300'>
+                    <div className='flex flex-row items-center w-full justify-center lg:space-x-200 md:space-x-100 space-x-50 '>
                         <LuCircleChevronLeft size={50} color='white' onClick={rotateLeft} className="cursor-pointer hover:scale-125 transition-all duration-300" />
                         <LuCircleChevronRight size={50} color='white' onClick={rotateRight} className="cursor-pointer hover:scale-125 transition-all duration-300" />
                     </div>
                 </div>
 
                 {/* วงกลม + รูป */}
-                <div className="relative flex justify-center items-center mt-50" data-aos="fade-up">
-                    <div className="w-1/2 h-screen rounded-full border-2 border-white border-dashed flex justify-center items-center relative">
-                        {/* map รูป */}
+                <div className="relative h-full  flex justify-center items-center" data-aos="fade-up">
+                    <div className="w-full lg:w-1/2 md:h-screen h-[50vh] md:mt-[10%] mt-[50%] rounded-full border-2 border-white border-dashed flex justify-center items-center relative">
+
                         {images.map((img, index) => {
-                            const total = images.length;
-                            const angle = (index * angleStep + rotation) % 360; // คำนวณมุมให้กลับไปที่ 0 เมื่อครบ 360 องศา
-                            const radius = 500; // ปรับขนาดวงกลมเล็กลง
+                            const angle = (index * angleStep + rotation) % 360;
                             const x = radius * Math.cos((angle * Math.PI) / 180);
                             const y = radius * Math.sin((angle * Math.PI) / 180);
 
@@ -105,7 +135,7 @@ const Homepage = () => {
                                     key={index}
                                     src={img}
                                     alt={`image-${index}`}
-                                    className="absolute w-80 h-80 rounded-full object-cover border-2 border-white transition-all duration-1000"
+                                    className="absolute w-40 md:w-80 h-40 md:h-80 rounded-full object-cover border-2 border-white transition-all duration-1000"
                                     style={{
                                         transform: `translate(${x}px, ${y}px)`,
                                     }}
@@ -114,6 +144,8 @@ const Homepage = () => {
                         })}
                     </div>
                 </div>
+
+
 
             </div>
         </div>
